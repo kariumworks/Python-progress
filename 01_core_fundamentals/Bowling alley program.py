5685b5dba34a305b4aabacedc4db3920b42a5143
kariumworks <159193170+kariumworks@users.noreply.github.com>
2026-06-18 19:05:22 +0100

    Delete 01_core_fundamentals/Bowling alley program.py

--- 01_core_fundamentals/Bowling alley program.py
+++
@@ -1,8 +0,0 @@
-#bowling alley program
-player_list = []
-playernames = input("Enter a player name: ")
-while playernames.lower() != "done":
-    player_list.append(playernames.title())
-    playernames = input("Enter a player name or type done to finish: ")
-print(f"There are {len(player_list)} players")
-print(f"Here is the list of players: {', '.join(player_list)}")
